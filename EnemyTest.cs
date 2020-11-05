20be6328e6f86dca1f17ccd569b05e351aabf279
Roman-Usaty <boss.eraser.a@gmail.com>
2020-11-05 10:11:08 +0400

    2335dgfgwd

---
+++ EnemyTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gemeTextConsole
+{
+    class EnemyTest : Enemy
+    {
+        private double regen = 1.4;
+        
+        public bool regeneration()
+        {
+            int health = GetHealth();
+            if ( health < 100)
+            {
+                int regenHealth = Math.floor(health * 1.4);
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
